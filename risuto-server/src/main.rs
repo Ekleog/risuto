22da0a6ec855048ffe20ac22da8b3b3ef716f6ff
Léo Gaspard <leo@gaspard.io>
2022-09-20 23:29:00 +0200

    attempt to actually use diesel

--- risuto-server/src/main.rs
+++ risuto-server/src/main.rs
@@ -1,7 +1,9 @@
 use anyhow::Context;
 use axum::{
-    http::StatusCode,
-    response::IntoResponse,
+    http::{self, StatusCode},
+    middleware::Next,
+    request::Request,
+    response::Response,
     routing::{get, post},
     Json, Router,
 };
@@ -12,6 +14,42 @@ use std::net::SocketAddr;
 mod models;
 mod schema;
 
+struct CurrentUser {
+    id: usize,
+}
+
+async fn auth<B>(mut req: Request<B>, next: Next<B>, Extension(connection): Extension<SqliteConnection>) -> Result<Response, StatusCode> {
+    let auth_header = req.headers()
+        .get(http::header::AUTHORIZATION)
+        .and_then(|header| header.to_str().ok())
+        .ok_or(StatusCode::UNAUTHORIZED)?;
+
+    if let Some(current_user) = authorize_current_user(auth_header).await {
+        req.extensions_mut().insert(current_user);
+        Ok(next.run(req).await)
+    } else {
+        Err(StatusCode::UNAUTHORIZED)
+    }
+}
+
+async fn authorize_current_user(auth_token: &str) -> Option<CurrentUser> {
+    let split = auth_token.split(" ");
+    if split.len() != 2 || split[0] != "Basic" {
+        return None;
+    }
+
+    let userpass = base64::decode(split[1])?;
+    let split = userpass.split(":");
+    if split.len() != 2 {
+        return None;
+    }
+
+    use diesel::prelude::*;
+    use self::schema::users::dsl::*;
+    let user = users.filter(name.eq(split[0])).load::<models::User>(connection)?;
+    Some(user.id)
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
@@ -20,7 +58,8 @@ async fn main() -> anyhow::Result<()> {
     let db = SqliteConnection::establish(&db_file)
         .with_context(|| format!("Error opening database {:?}", db_file))?;
 
-    let app = Router::new().route("/", get(root));
+    let app = Router::new().route("/", get(root))
+    .route_layer(axum::middleware::from_fn(auth));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::info!("listening on {}", addr);
@@ -30,7 +69,7 @@ async fn main() -> anyhow::Result<()> {
         .context("serving axum webserver")
 }
 
-// basic handler that responds with a static string
-async fn root() -> &'static str {
+// basic handler that responds with a static string, but only to auth'd users
+async fn root(Extension(current_user): Extension<CurrentUser>) -> &'static str {
     "Hello, World!"
 }
