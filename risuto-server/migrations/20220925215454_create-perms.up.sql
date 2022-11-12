9f2a452b1f7d9ec68f85f81b244d46be8d093a40
Léo Gaspard <leo@gaspard.io>
2022-11-12 18:06:46 +0100

    a line in perms always means can_read

--- risuto-server/migrations/20220925215454_create-perms.up.sql
+++ risuto-server/migrations/20220925215454_create-perms.up.sql
@@ -2,17 +2,10 @@ CREATE TABLE perms (
     tag_id UUID NOT NULL,
     user_id UUID NOT NULL,
 
-    can_read BOOLEAN NOT NULL, -- read tasks
+    -- can always read tasks when a row exists
     can_tag BOOLEAN NOT NULL, -- add/remove tasks that user_id owns to tag
     can_comment BOOLEAN NOT NULL, -- add comments to task
     can_close BOOLEAN NOT NULL, -- close a task as done
-
-    CONSTRAINT at_least_one_perm CHECK (
-        can_read != false OR
-        can_tag != false OR
-        can_comment != false OR
-        can_close != false
-    ),
 
     PRIMARY KEY (tag_id, user_id),
     FOREIGN KEY (tag_id) REFERENCES tags (id)
