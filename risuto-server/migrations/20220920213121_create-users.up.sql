2f73ed05ef480ca556532d5f187609c276d33c7c
Léo Gaspard <leo@gaspard.io>
2022-11-14 00:36:47 +0100

    validate username

--- risuto-server/migrations/20220920213121_create-users.up.sql
+++ risuto-server/migrations/20220920213121_create-users.up.sql
@@ -1,5 +1,7 @@
 CREATE TABLE users (
     id UUID PRIMARY KEY NOT NULL,
     name VARCHAR NOT NULL UNIQUE,
-    password VARCHAR NOT NULL
+    password VARCHAR NOT NULL,
+
+    CHECK (name ~ '^[a-zA-Z0-9]+$')
 );
