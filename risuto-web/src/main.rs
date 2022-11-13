23797252187c263abcac9eadf99eb4baab75c04b
Léo Gaspard <leo@gaspard.io>
2022-11-13 18:22:28 +0100

    make App into a struct component

--- risuto-web/src/main.rs
+++ risuto-web/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use chrono::Utc;
 use risuto_api::*;
 use yew::prelude::*;
@@ -6,44 +8,65 @@ fn main() {
     yew::start_app::<App>();
 }
 
-#[function_component(App)]
-fn app() -> Html {
-    let mut tasks = Vec::new();
-    tasks.push((TaskId(Uuid::new_v4()), Task {
-        owner: UserId(Uuid::new_v4()),
-        date: Utc::now(),
-        initial_title: "Task 1".to_string(),
-        current_title: "Task 1".to_string(),
-        is_done: false,
-        is_archived: false,
-        scheduled_for: None,
-        current_tags: std::collections::HashMap::new(),
-        deps_before_self: std::collections::HashSet::new(),
-        deps_after_self: std::collections::HashSet::new(),
-        current_comments: std::collections::BTreeMap::new(),
-        events: std::collections::BTreeMap::new(),
-    }));
-    tasks.push((TaskId(Uuid::new_v4()), Task {
-        owner: UserId(Uuid::new_v4()),
-        date: Utc::now(),
-        initial_title: "Task 2".to_string(),
-        current_title: "Task 2 new title".to_string(),
-        is_done: false,
-        is_archived: false,
-        scheduled_for: None,
-        current_tags: std::collections::HashMap::new(),
-        deps_before_self: std::collections::HashSet::new(),
-        deps_after_self: std::collections::HashSet::new(),
-        current_comments: std::collections::BTreeMap::new(),
-        events: std::collections::BTreeMap::new(),
-    }));
-    html! {
-        <>
-            <h1>{ "Tasks" }</h1>
-            <ul class="list-group">
-                <TaskList tasks={tasks} />
-            </ul>
-        </>
+struct App {
+    db: DbDump,
+}
+
+impl Component for App {
+    type Message = ();
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        let users = HashMap::new();
+        let tags = HashMap::new();
+        let mut tasks = HashMap::new();
+        tasks.insert(
+            TaskId(Uuid::new_v4()),
+            Task {
+                owner: UserId(Uuid::new_v4()),
+                date: Utc::now(),
+                initial_title: "Task 1".to_string(),
+                current_title: "Task 1".to_string(),
+                is_done: false,
+                is_archived: false,
+                scheduled_for: None,
+                current_tags: std::collections::HashMap::new(),
+                deps_before_self: std::collections::HashSet::new(),
+                deps_after_self: std::collections::HashSet::new(),
+                current_comments: std::collections::BTreeMap::new(),
+                events: std::collections::BTreeMap::new(),
+            },
+        );
+        tasks.insert(
+            TaskId(Uuid::new_v4()),
+            Task {
+                owner: UserId(Uuid::new_v4()),
+                date: Utc::now(),
+                initial_title: "Task 2".to_string(),
+                current_title: "Task 2 new title".to_string(),
+                is_done: false,
+                is_archived: false,
+                scheduled_for: None,
+                current_tags: std::collections::HashMap::new(),
+                deps_before_self: std::collections::HashSet::new(),
+                deps_after_self: std::collections::HashSet::new(),
+                current_comments: std::collections::BTreeMap::new(),
+                events: std::collections::BTreeMap::new(),
+            },
+        );
+        Self { db: DbDump { users, tags, tasks } }
+    }
+
+    fn view(&self, _ctx: &Context<Self>) -> Html {
+        let tasks = self.db.tasks.iter().map(|(id, task)| (*id, task.clone())).collect::<Vec<_>>();
+        html! {
+            <>
+                <h1>{ "Tasks" }</h1>
+                <ul class="list-group">
+                    <TaskList tasks={tasks} />
+                </ul>
+            </>
+        }
     }
 }
 
