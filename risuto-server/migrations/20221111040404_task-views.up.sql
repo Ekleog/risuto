2c8ad5bdee19a1749da1c179b31bb177f2b2dd3a
Léo Gaspard <leo@gaspard.io>
2022-11-30 01:58:01 +0100

    add a v_tags_users view

--- risuto-server/migrations/20221111040404_task-views.up.sql
+++ risuto-server/migrations/20221111040404_task-views.up.sql
@@ -30,6 +30,43 @@ WHERE NOT EXISTS (
     AND rte.date > ate.date
 );
 
+CREATE VIEW v_tags_users AS
+SELECT
+    tag_id,
+    user_id,
+    BOOL_OR(can_edit) AS can_edit,
+    BOOL_OR(can_triage) AS can_triage,
+    BOOL_OR(can_relabel_to_any) AS can_relabel_to_any,
+    BOOL_OR(can_comment) AS can_comment
+FROM (
+    (
+        SELECT
+            id as tag_id,
+            owner_id as user_id,
+            true as can_edit,
+            true as can_triage,
+            true as can_relabel_to_any,
+            true as can_comment
+        FROM
+            tags
+    )
+    UNION
+    (
+        SELECT
+            t.id as tag_id,
+            p.user_id as user_id,
+            p.can_edit,
+            p.can_triage,
+            p.can_relabel_to_any,
+            p.can_comment
+        FROM
+            tags t
+        INNER JOIN perms p
+            ON p.tag_id = t.id
+    )
+) AS the_table_because_postgres_needs_this_name
+GROUP BY (tag_id, user_id);
+
 CREATE VIEW v_tasks_users AS
 SELECT
     task_id,
@@ -54,33 +91,17 @@ FROM (
     (
         SELECT
             t.id as task_id,
-            tag.owner_id as user_id,
-            true as can_edit,
-            true as can_triage,
-            true as can_relabel_to_any,
-            true as can_comment
+            vtu.user_id as user_id,
+            vtu.can_edit,
+            vtu.can_triage,
+            vtu.can_relabel_to_any,
+            vtu.can_comment
         FROM
             tasks t
         INNER JOIN v_tasks_tags vtt
             ON vtt.task_id = t.id
-        INNER JOIN tags tag
-            ON tag.id = vtt.tag_id
-    )
-    UNION
-    (
-        SELECT
-            t.id as task_id,
-            p.user_id as user_id,
-            p.can_edit,
-            p.can_triage,
-            p.can_relabel_to_any,
-            p.can_comment
-        FROM
-            tasks t
-        INNER JOIN v_tasks_tags vtt
-            ON vtt.task_id = t.id
-        INNER JOIN perms p
-            ON p.tag_id = vtt.tag_id
+        INNER JOIN v_tags_users vtu
+            ON vtu.tag_id = vtt.tag_id
     )
 ) AS the_table_because_postgres_needs_this_name
 GROUP BY (task_id, user_id);
