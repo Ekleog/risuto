09af12e5e21813a7033fe53d9b85c69c7abf01ce
Léo Gaspard <leo@gaspard.io>
2022-12-09 03:03:26 +0100

    split title div building to separate struct

--- risuto-web/src/ui/task_list_item.rs
+++ risuto-web/src/ui/task_list_item.rs
@@ -12,57 +12,57 @@ pub struct TaskListItemProps {
 pub fn task_list(p: &TaskListItemProps) -> Html {
     let title_edit = use_state(|| None);
 
-    let title_div = {
-        let current_title = p.task.current_title.clone();
-        let on_validate = {
-            let title_edit = title_edit.clone();
-            p.on_title_change.reform(move |t| {
-                title_edit.set(None);
-                t
-            })
-        };
-        match (*title_edit).clone() {
-            None => html! {
-                <div
-                    class="flex-fill d-flex align-items-center"
-                    ondblclick={ Callback::from(move |_| {
-                        title_edit.set(Some(current_title.clone()))
-                    }) }
-                >
-                    { &p.task.current_title }
-                </div>
-            },
-            Some(t) => html! {
-                <div class="flex-fill d-flex align-items-center">
-                    <input
-                        type="text"
-                        value={ t.clone() }
-                        onchange={ Callback::from(move |e: web_sys::Event| {
-                            let input: web_sys::HtmlInputElement = e.target_unchecked_into();
-                            title_edit.set(Some(input.value()))
-                        }) }
-                        onfocusout={ let t = t.clone(); on_validate.reform(move |_| t.clone()) }
-                        onkeyup={ Callback::from(move |e: web_sys::KeyboardEvent| {
-                            if e.key() == "Enter" {
-                                on_validate.emit(t.clone())
-                            }
-                        }) }
-                    />
-                </div>
-            },
-        }
-    };
-
     html! { // align items vertically but also let them stretch
         <li class="list-group-item d-flex align-items-stretch">
             <div class="drag-handle d-flex align-items-center">
                 <div class="bi-btn bi-grip-vertical pe-3"></div>
             </div>
-            { title_div }
+            { title_div(&p, title_edit) }
             <div class="d-flex align-items-center">
                 { button_done_change(&p.task, &p.on_done_change) }
             </div>
         </li>
+    }
+}
+
+fn title_div(p: &TaskListItemProps, edit: UseStateHandle<Option<String>>) -> Html {
+    let current_title = p.task.current_title.clone();
+    let on_validate = {
+        let edit = edit.clone();
+        p.on_title_change.reform(move |t| {
+            edit.set(None);
+            t
+        })
+    };
+    match (*edit).clone() {
+        None => html! {
+            <div
+                class="flex-fill d-flex align-items-center"
+                ondblclick={ Callback::from(move |_| {
+                    edit.set(Some(current_title.clone()))
+                }) }
+            >
+                { &p.task.current_title }
+            </div>
+        },
+        Some(t) => html! {
+            <div class="flex-fill d-flex align-items-center">
+                <input
+                    type="text"
+                    value={ t.clone() }
+                    onchange={ Callback::from(move |e: web_sys::Event| {
+                        let input: web_sys::HtmlInputElement = e.target_unchecked_into();
+                        edit.set(Some(input.value()))
+                    }) }
+                    onfocusout={ let t = t.clone(); on_validate.reform(move |_| t.clone()) }
+                    onkeyup={ Callback::from(move |e: web_sys::KeyboardEvent| {
+                        if e.key() == "Enter" {
+                            on_validate.emit(t.clone())
+                        }
+                    }) }
+                />
+            </div>
+        },
     }
 }
 
