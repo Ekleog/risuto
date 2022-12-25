3db70b26aab4009114763e0b1e56c5ff8d625e6c
Léo Gaspard <leo@gaspard.io>
2022-12-26 00:06:51 +0100

    remove useless joins in views

--- risuto-server/migrations/20221111040404_task-views.up.sql
+++ risuto-server/migrations/20221111040404_task-views.up.sql
@@ -1,24 +1,20 @@
 CREATE VIEW v_tasks_archived AS
-SELECT DISTINCT ON (t.id)
-    t.id AS task_id,
+SELECT DISTINCT ON (e.task_id)
+    e.task_id AS task_id,
     e.d_bool AS archived
-FROM tasks t
-INNER JOIN events e
-    ON e.task_id = t.id
+FROM events e
 WHERE e.d_type = 'set_archived'
-ORDER BY t.id, e.date DESC;
+ORDER BY e.task_id, e.date DESC;
 
 CREATE VIEW v_tasks_comments AS
 SELECT DISTINCT ON (task_id, comment_id)
-    t.id AS task_id,
+    e.task_id AS task_id,
     (CASE e.d_type
         WHEN 'add_comment' THEN e.id
         ELSE e.d_parent_id
     END) AS comment_id,
     e.d_text AS text
-FROM tasks t
-INNER JOIN events e
-    ON e.task_id = t.id
+FROM events e
 WHERE (e.d_type = 'add_comment' OR e.d_type = 'edit_comment')
 ORDER BY task_id, comment_id, e.date DESC;
 
@@ -30,16 +26,13 @@ FROM v_tasks_comments
 GROUP BY task_id;
 
 CREATE VIEW v_tasks_tags AS
-SELECT DISTINCT ON (t.id, e.d_tag_id)
-    t.id AS task_id,
+SELECT DISTINCT ON (task_id, tag_id)
+    e.task_id AS task_id,
     e.d_tag_id AS tag_id,
     (e.d_type = 'add_tag') AS is_in
-FROM
-    tasks t
-INNER JOIN events e
-    ON e.task_id = t.id
+FROM events e
 WHERE (e.d_type = 'add_tag' OR e.d_type = 'remove_tag')
-ORDER BY t.id, e.d_tag_id, e.date DESC;
+ORDER BY task_id, tag_id, e.date DESC;
 
 CREATE VIEW v_tags_users AS
 SELECT
