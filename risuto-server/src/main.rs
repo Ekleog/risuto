e3d5a07b46aaba41030a2070aeb7cd5b92720f61
Léo Gaspard <leo@gaspard.io>
2023-01-20 18:47:32 +0100

    move user feeds to separate file

--- risuto-server/src/main.rs
+++ risuto-server/src/main.rs
@@ -4,22 +4,22 @@ use axum::{
     routing::{get, post},
     Json, Router,
 };
-use futures::{channel::mpsc, select, stream, SinkExt, Stream, StreamExt};
+use futures::{SinkExt, StreamExt};
 use risuto_api::Error as ApiError;
 use risuto_api::{
-    Action, AuthInfo, AuthToken, Event, FeedMessage, NewSession, NewUser, Search, Tag, Task, User,
-    UserId, Uuid,
+    Action, AuthInfo, AuthToken, Event, NewSession, NewUser, Search, Tag, Task, User, UserId, Uuid,
 };
-use std::{collections::HashMap, iter, net::SocketAddr, pin::Pin, sync::Arc};
-use tokio::sync::RwLock;
+use std::net::SocketAddr;
 use tower_http::trace::TraceLayer;
 
 mod db;
 mod extractors;
+mod feeds;
 mod fuzz;
 mod query;
 
 use extractors::*;
+use feeds::UserFeeds;
 
 #[derive(Debug, structopt::StructOpt)]
 struct Opt {
@@ -77,7 +77,7 @@ async fn main() -> anyhow::Result<()> {
 }
 
 async fn app(db: PgPool, admin_token: Option<AuthToken>) -> Router {
-    let feeds = UserFeeds(Arc::new(RwLock::new(HashMap::new())));
+    let feeds = UserFeeds::new();
 
     let state = AppState {
         db,
@@ -296,122 +296,5 @@ where
         let _ = write
             .send(Message::Text(String::from("permission denied")))
             .await;
-    }
-}
-
-#[derive(Clone, Debug)]
-pub struct UserFeeds(
-    Arc<RwLock<HashMap<UserId, HashMap<Uuid, mpsc::UnboundedSender<FeedMessage>>>>>,
-);
-
-impl UserFeeds {
-    async fn add_for_user<W, R>(self, user: UserId, mut write: W, read: R)
-    where
-        W: 'static + Send + Unpin + futures::Sink<Message>,
-        <W as futures::Sink<Message>>::Error: Send,
-        R: 'static + Send + Unpin + futures::Stream<Item = Result<Message, axum::Error>>,
-    {
-        // Create relayer channel
-        // Note: if this were bounded, there would be a deadlock between the write-lock to remove a channel and the read-lock to send an event to all interested sockets
-        let (sender, mut receiver) = mpsc::unbounded();
-        let sender_id = Uuid::new_v4();
-
-        // Add relayer endpoint to hashmap
-        // TODO: limit to some reasonable number of sockets, to avoid starvations
-        self.0
-            .write()
-            .await
-            .entry(user)
-            .or_insert_with(HashMap::new)
-            .insert(sender_id, sender);
-
-        // Start relayer queue
-        let this = self.clone();
-        let user = user.clone();
-        let mut read = read.fuse();
-        tokio::spawn(async move {
-            macro_rules! remove_self {
-                () => {{
-                    this.0
-                        .write()
-                        .await
-                        .get_mut(&user)
-                        .expect("user {user:?} disappeared")
-                        .remove(&sender_id);
-                    return;
-                }};
-            }
-            macro_rules! send_message {
-                ( $msg:expr ) => {{
-                    let msg: FeedMessage = $msg;
-                    let json = match serde_json::to_vec(&msg) {
-                        Ok(json) => json,
-                        Err(err) => {
-                            tracing::error!(?err, ?msg, "failed serializing message to json");
-                            continue;
-                        }
-                    };
-                    if let Err(_) = write.send(Message::Binary(json)).await {
-                        // TODO: check error details, using axum-tungstenite, to confirm we need to remove this socket
-                        remove_self!();
-                    }
-                }};
-            }
-            loop {
-                select! {
-                    msg = receiver.next() => match msg {
-                        None => remove_self!(),
-                        Some(msg) => send_message!(msg),
-                    },
-                    msg = read.next() => match msg {
-                        None => remove_self!(),
-                        Some(Ok(Message::Text(msg))) => {
-                            if msg != "ping" {
-                                tracing::warn!("received unexpected message from client: {msg:?}");
-                                remove_self!();
-                            }
-                            send_message!(FeedMessage::Pong);
-                        }
-                        Some(Ok(Message::Close(_))) => remove_self!(),
-                        Some(msg) => {
-                            tracing::warn!("received unexpected message from client: {msg:?}");
-                            remove_self!();
-                        }
-                    },
-                }
-            }
-        });
-    }
-
-    async fn relay_action(&self, conn: &mut sqlx::PgConnection, a: Action) {
-        match &a {
-            Action::NewUser(_) => match db::fetch_users(conn).await {
-                Err(e) => Box::pin(stream::iter(iter::once(Err(e))))
-                    as Pin<Box<dyn Send + Stream<Item = anyhow::Result<UserId>>>>,
-                Ok(u) => Box::pin(stream::iter(u.into_iter().map(|u| Ok(u.id)))),
-            },
-            Action::NewTask(t, _) => Box::pin(stream::iter(iter::once(Ok(t.owner_id)))),
-            Action::NewEvent(e) => Box::pin(db::users_interested_by(conn, &[e.task_id.0])),
-            // TODO: make sure we actually send the whole task if a user gets access to this task it didn't have before
-        }
-        // TODO: magic numbers below should be at least explained
-        .for_each_concurrent(Some(16), |u| {
-            let a = a.clone();
-            async move {
-                match u {
-                    Err(err) => {
-                        tracing::error!(?err, "error occurred while listing interested users");
-                    }
-                    Ok(u) => {
-                        if let Some(socks) = self.0.read().await.get(&u) {
-                            for s in socks.values() {
-                                let _ = s.unbounded_send(FeedMessage::Action(a.clone()));
-                            }
-                        }
-                    }
-                }
-            }
-        })
-        .await;
     }
 }
